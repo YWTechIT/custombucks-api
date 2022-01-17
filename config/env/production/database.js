c5c13bc73c1d38bbce01e24f7ccc64381c2e27ce
YWTechIT <abcd8637@naver.com>
2022-01-17 22:31:54 +0900

    change username

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -11,7 +11,7 @@ module.exports = ({ env }) => ({
         host: config.host,
         port: config.port,
         database: config.database,
-        user: config.user,
+        username: config.user,
         password: config.password,
         ssl: {
           rejectUnauthorized: false,
